138ead137879b3ea0098795418bbe4609308e5fc
Marek Dědič <developer@dedic.eu>
2024-06-05 00:35:23 +0200

    Building the plugin

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "src/index.ts",
+  external: ["fs", "path"],
+  output: [
+    {
+      file: "dist/index.cjs",
+      format: "cjs",
+      compact: true,
+    },
+    {
+      file: "dist/index.js",
+      format: "es",
+      compact: true,
+    },
+  ],
+  plugins: [typescript()],
+};
