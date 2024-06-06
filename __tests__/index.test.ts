23d8c12cf0a3daa3d19268f522d6e12288b8ed75
Marek Dědič <developer@dedic.eu>
2024-06-06 19:11:05 +0200

    Added basic test with vite

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,7 +1,20 @@
-import { compileRollup } from "./utils";
+import { compileRollup, compileVite } from "./utils";
 
 test("Basic rollup", async () => {
   const htaccess = await compileRollup({
+    headers: [
+      {
+        header: "X-Frame-Options",
+        action: "set",
+        value: "deny",
+      },
+    ],
+  });
+  expect(htaccess).toBe("Header set X-Frame-Options DENY\n");
+});
+
+test("Basic vite", async () => {
+  const htaccess = await compileVite({
     headers: [
       {
         header: "X-Frame-Options",
--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -1,6 +1,16 @@
-import { type OutputAsset, rollup } from "rollup";
+import { type OutputAsset, rollup, type RollupOutput } from "rollup";
+import { build } from "vite";
 
 import htaccess, { type Options } from "../src";
+
+function extractFileContents(output: RollupOutput): string {
+  const htaccessFiles = output.output.filter(
+    (file): file is OutputAsset =>
+      file.type === "asset" && file.fileName === ".htaccess",
+  );
+  expect(htaccessFiles).toHaveLength(1);
+  return htaccessFiles[0].source.toString();
+}
 
 export async function compileRollup(
   options?: Partial<Options>,
@@ -9,11 +19,21 @@ export async function compileRollup(
     input: "__tests__/fixtures/dummy.js",
     plugins: [htaccess(options)],
   });
-  const { output } = await bundle.generate({});
-  const htaccessFiles = output.filter(
-    (file): file is OutputAsset =>
-      file.type === "asset" && file.fileName === ".htaccess",
-  );
-  expect(htaccessFiles).toHaveLength(1);
-  return htaccessFiles[0].source.toString();
+  const output = await bundle.generate({});
+  return extractFileContents(output);
+}
+
+export async function compileVite(options?: Partial<Options>): Promise<string> {
+  const output = (await build({
+    logLevel: "warn",
+    build: {
+      rollupOptions: {
+        input: {
+          app: "__tests__/fixtures/dummy.html",
+        },
+      },
+    },
+    plugins: [htaccess(options)],
+  })) as Array<RollupOutput> | RollupOutput;
+  return extractFileContents(Array.isArray(output) ? output[0] : output);
 }
