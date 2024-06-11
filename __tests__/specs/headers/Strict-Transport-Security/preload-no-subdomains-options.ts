927c0083b7c55b7aabfff202719b59903215c342
Marek Dědič <developer@dedic.eu>
2024-06-11 16:12:24 +0200

    Added tests for STS errors

---
+++ __tests__/specs/headers/Strict-Transport-Security/preload-no-subdomains-options.ts
@@ -0,0 +1,11 @@
+import type { Options } from "../../../../src";
+
+export default {
+  headers: [
+    {
+      action: "set",
+      header: "Strict-Transport-Security",
+      value: { maxAge: 31536000, preload: true },
+    },
+  ],
+} as Partial<Options>;
